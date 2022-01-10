fa1c6d68f057dbf59896d32ef6923fe2f5fc0a63
gitkidmk <lovekangmk@gmail.com>
2022-01-11 03:14:51 +0900

    [feat] new project

---
+++ src/main/java/com/mkkang/mbti_with_music/MbtiWithMusicApplication.java
@@ -0,0 +1,13 @@
+package com.mkkang.mbti_with_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiWithMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiWithMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkkang/mbti_with_music/MbtiWithMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkkang.mbti_with_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiWithMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
