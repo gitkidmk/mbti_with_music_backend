3d4f2b4f453573545cf10b9b78d05de13b227fca
gitkidmk <lovekangmk@gmail.com>
2022-01-12 01:03:18 +0900

    [feat] mapper works with xml
    - api 명세 보고 하나씩 생성

--- src/main/java/com/mkkang/mbti_with_music/mapper/MainMapper.java
+++ src/main/java/com/mkkang/mbti_with_music/mapper/MainMapper.java
@@ -5,10 +5,8 @@ import java.util.List;
 import com.mkkang.mbti_with_music.domain.MusicInfo;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface MainMapper {
-    @Select("select * from music")
     List<MusicInfo> allMusic();
 }
