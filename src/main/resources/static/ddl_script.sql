d9824e5020aa53b1be7adc0f3a92e23becf7227d
gitkidmk <lovekangmk@gmail.com>
2022-01-16 23:22:59 +0900

    [chore] ddl, gitignore 수정

--- src/main/resources/static/ddl_script.sql
+++ src/main/resources/static/ddl_script.sql
@@ -2,6 +2,72 @@ show databases;
 
 create database mbti_music;
 
+-- mbti_music.mbti_music_user definition
+
+CREATE TABLE `mbti_music_user` (
+  `music_id` varchar(1000) NOT NULL,
+  `mbti_name` char(4) DEFAULT NULL,
+  `album` varchar(1000) DEFAULT NULL,
+  `artist` varchar(1000) DEFAULT NULL,
+  `session_id` varchar(1000) DEFAULT NULL,
+  `mbti_music_user_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`mbti_music_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+
+
+-- mbti_music.mbti_result definition
+
+CREATE TABLE `mbti_result` (
+  `result_id` int(11) NOT NULL,
+  `mbti` char(4) DEFAULT NULL,
+  `EI` float DEFAULT NULL,
+  `SN` float DEFAULT NULL,
+  `TF` float DEFAULT NULL,
+  `JP` float DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`result_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+-- mbti_music.music definition
+
+CREATE TABLE `music` (
+  `music_id` varchar(1000) NOT NULL,
+  `album` varchar(1000) DEFAULT NULL,
+  `artist` varchar(1000) DEFAULT NULL,
+  `music_name` varchar(1000) NOT NULL,
+  PRIMARY KEY (`music_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+
+-- mbti_music.mbti_music_great definition
+
+CREATE TABLE `mbti_music_great` (
+  `music_id` varchar(1000) NOT NULL,
+  `session_id` varchar(1000) DEFAULT NULL,
+  `mbti_music_great_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mbti_name` char(4) NOT NULL,
+  PRIMARY KEY (`mbti_music_great_id`),
+  KEY `FK_music_TO_mbti_music_great_2` (`music_id`),
+  CONSTRAINT `FK_music_TO_mbti_music_great_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
+
+
+-- mbti_music.music_mbti_rel definition
+
+CREATE TABLE `music_mbti_rel` (
+  `music_mbti_id` int(11) NOT NULL AUTO_INCREMENT,
+  `music_id` varchar(1000) NOT NULL,
+  `mbti_name` char(4) DEFAULT NULL,
+  PRIMARY KEY (`music_mbti_id`),
+  KEY `FK_music_TO_music_mbti_rel_1` (`music_id`),
+  CONSTRAINT `FK_music_TO_music_mbti_rel_1` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+
+-----------------------
+-- old table ddl
+-----------------------
+
 CREATE TABLE `mbti_result` (
 	`result_id`	INT	NOT NULL,
 	`mbti`	CHAR(4)	NULL,
