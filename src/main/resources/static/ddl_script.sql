8cac507b86ca1778cce4aeea3344d5359f84b2f0
gitkidmk <lovekangmk@gmail.com>
2022-03-06 13:21:09 +0900

    [chore] change ddl script

--- src/main/resources/static/ddl_script.sql
+++ src/main/resources/static/ddl_script.sql
@@ -1,14 +1,15 @@
 create database mbti_music;
+use mbti_music;
 
 -- mbti_music.mbti_music_user definition
 
 CREATE TABLE `mbti_music_user` (
-  `mbti_music_user_id` int(11) NOT NULL AUTO_INCREMENT,
   `music_id` varchar(1000) NOT NULL,
   `mbti_name` char(4) DEFAULT NULL,
   `album` varchar(1000) DEFAULT NULL,
   `artist` varchar(1000) DEFAULT NULL,
   `session_id` varchar(1000) DEFAULT NULL,
+  `mbti_music_user_id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`mbti_music_user_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb3;
 
@@ -36,16 +37,16 @@ CREATE TABLE `mbti_result` (
   `JP` float DEFAULT NULL,
   `time` datetime DEFAULT NULL,
   PRIMARY KEY (`result_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
 
 -- mbti_music.music definition
 
 CREATE TABLE `music` (
   `music_id` varchar(1000) NOT NULL,
-  `music_name` varchar(1000) DEFAULT NULL,
   `album` varchar(1000) DEFAULT NULL,
   `artist` varchar(1000) DEFAULT NULL,
+  `music_name` varchar(1000) DEFAULT NULL,
   `thumbnail` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`music_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
@@ -54,14 +55,14 @@ CREATE TABLE `music` (
 -- mbti_music.mbti_music_great definition
 
 CREATE TABLE `mbti_music_great` (
-  `mbti_music_great_id` int(11) NOT NULL AUTO_INCREMENT,
   `music_id` varchar(1000) NOT NULL,
-  `mbti_name` char(4) NOT NULL,
   `session_id` varchar(1000) DEFAULT NULL,
+  `mbti_music_great_id` int(11) NOT NULL AUTO_INCREMENT,
+  `mbti_name` char(4) NOT NULL,
   PRIMARY KEY (`mbti_music_great_id`),
   KEY `FK_music_TO_mbti_music_great_2` (`music_id`),
   CONSTRAINT `FK_music_TO_mbti_music_great_2` FOREIGN KEY (`music_id`) REFERENCES `music` (`music_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=64 DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8mb3;
 
 
 -- mbti_music.music_mbti_rel definition
